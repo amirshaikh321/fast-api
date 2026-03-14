89da3544182bcd3c0d37cb6c50701e88832e0ca9
Amir Shaikh <amirshaikh1594@gmail.com>
2026-03-14 17:42:21 +0530

    specific patient details access

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 import json
 
 app = FastAPI()
@@ -20,3 +20,10 @@ def about():
 def view():
     data = load_data()
     return data
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id:str =  Path(...,description='Id of the patient in DB', example='P001`')):
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
+    return "error the Id is not in DB"
