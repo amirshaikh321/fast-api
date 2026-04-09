a2650d329eb45ad05af068591adbba21c5a3ff2a
Amir Shaikh <amirshaikh1594@gmail.com>
2026-04-09 20:01:01 +0530

    data validation using pydantic

---
+++ pydantic_tutorial/data_validation.py
@@ -0,0 +1,59 @@
+from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator, model_validator
+from typing import List, Dict, Optional, Annotated
+
+class Patient(BaseModel):
+    name: Annotated[str, Field(title='Name of the Patient',description='Give the name of the patient in less than 50 characters',examples=['Karishma','Max'])]
+    age: Annotated[int, Field(gt=0,lt =120,description='give the patient age')]
+    email : EmailStr
+    linkedin : AnyUrl
+    weight: float = Field(gt=0)
+    married: bool
+    allergies : Optional[List[str]] = None
+    contact_info : Dict[str, str]
+
+    @field_validator('email')
+    @classmethod
+    def email_validator(cls, value):
+        valid_domains = ['hdfc.com','icici.com']
+        domain_name = value.split('@')[-1]
+        if domain_name not in valid_domains:
+            raise ValueError('Not a valid Domain')
+        
+        return value
+    
+    @field_validator('name')
+    @classmethod
+    def transform(cls,name):
+        return name.upper()
+
+    @model_validator(mode='after')
+    def validate_emergency_contact(cls,model):
+        if model.age >60 and 'emergency' not in model.contact_info:
+            raise ValueError("Patient older than 60 must have emergency contact")
+        return model
+    
+
+
+def insert_data(patient: Patient):
+    print(patient.name)
+    print(patient.age)
+    print(patient.email)
+    print(patient.linkedin)
+    print(patient.weight)
+    print(patient.married)
+    print(patient.allergies)
+    print(patient.contact_info)
+    print("inserted")
+
+patient_info = {"name":"Aman",
+    "age":87,
+    "email":'amirshaikh1594@hdfc.com',
+    "linkedin":'https://www.linkedin.com/in/amir-shaikh-75aaa1251',
+    "weight":65.5,
+    "married":False,
+    "allergies":['pollen','dust'],
+    "contact_info":{'Phone':'8421652597','emergency':'56886539'}}
+
+patient1 = Patient(**patient_info)
+
+insert_data(patient1)
